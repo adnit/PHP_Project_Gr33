64ca9b60d2c9979c804ce6d97715e2d14dfadfeb
adniti <adnit.kamberi@student.uni-pr.edu>
2021-05-21 01:33:15 +0200

    added movie.php

---
+++ movie1.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+  <title>Dashboard - Brand</title>
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/3.0.1/iconfont/material-icons.min.css">
+  <link rel="stylesheet" href="assets/fonts/fontawesome5-overrides.min.css">
+  <link rel="stylesheet" href="assets/css/styles.min.css">
+</head>
+
+<body id="page-top">
+  <div id="wrapper">
+    <nav class="navbar navbar-dark align-items-start sidebar sidebar-dark accordion bg-gradient-primary p-0">
+      <div class="container-fluid d-flex flex-column p-0"><a class="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0" href="#">
+          <div class="sidebar-brand-icon rotate-n-15"><i class="material-icons">local_movies</i></div>
+          <div class="sidebar-brand-text mx-3"><span>kinofiek</span></div>
+        </a>
+        <hr class="sidebar-divider my-0">
+        <ul class="navbar-nav text-light" id="accordionSidebar">
+          <li class="nav-item"><a class="nav-link" href="index.html"><i class="fas fa-tachometer-alt"></i><span>Ballina</span></a></li>
+          <li class="nav-item"><a class="nav-link" href="evente.html"><i class="fas fa-user"></i><span>Evente</span></a></li>
+          <li class="nav-item"><a class="nav-link" href="sallat.html"><i class="fas fa-table"></i><span>Sallat</span></a></li>
+          <li class="nav-item"><a class="nav-link" href="kontakto.html"><i class="far fa-user-circle"></i><span>Kontakti</span></a></li>
+          <li class="nav-item"><a class="nav-link" href="about%20EDIT!!!.html"><i class="fas fa-user-circle"></i><span>Rreth kinemase tone</span></a></li>
+        </ul>
+        <div class="text-center d-none d-md-inline"><button class="btn rounded-circle border-0" id="sidebarToggle" type="button"></button></div>
+      </div>
+    </nav>
+    <div class="d-flex flex-column" id="content-wrapper">
+      <div id="content">
+        <nav class="navbar navbar-light navbar-expand bg-white shadow mb-4 topbar static-top">
+          <div class="container-fluid"><button class="btn btn-link d-md-none rounded-circle me-3" id="sidebarToggleTop" type="button"><i class="fas fa-bars"></i></button>
+            <form class="d-none d-sm-inline-block me-auto ms-md-3 my-2 my-md-0 mw-100 navbar-search">
+              <div class="input-group"><input class="bg-light form-control border-0 small" type="text" placeholder="Search for ..."><button class="btn btn-primary py-0" type="button"><i class="fas fa-search"></i></button></div>
+            </form>
+            <ul class="navbar-nav flex-nowrap ms-auto">
+              <li class="nav-item dropdown d-sm-none no-arrow"><a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#"><i class="fas fa-search"></i></a>
+                <div class="dropdown-menu dropdown-menu-end p-3 animated--grow-in" aria-labelledby="searchDropdown">
+                  <form class="me-auto navbar-search w-100">
+                    <div class="input-group"><input class="bg-light form-control border-0 small" type="text" placeholder="Search for ...">
+                      <div class="input-group-append"><button class="btn btn-primary py-0" type="button"><i class="fas fa-search"></i></button></div>
+                    </div>
+                  </form>
+                </div>
+              </li>
+              <li class="nav-item dropdown no-arrow mx-1">
+                <div class="nav-item dropdown no-arrow"><a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#"><span class="badge bg-danger badge-counter">3+</span><i class="fas fa-bell fa-fw"></i></a>
+                  <div class="dropdown-menu dropdown-menu-end dropdown-list animated--grow-in">
+                    <h6 class="dropdown-header">alerts center</h6><a class="dropdown-item d-flex align-items-center" href="#">
+                      <div class="me-3">
+                        <div class="bg-primary icon-circle"><i class="fas fa-file-alt text-white"></i></div>
+                      </div>
+                      <div><span class="small text-gray-500">December 12, 2019</span>
+                        <p>A new monthly report is ready to download!</p>
+                      </div>
+                    </a><a class="dropdown-item d-flex align-items-center" href="#">
+                      <div class="me-3">
+                        <div class="bg-success icon-circle"><i class="fas fa-donate text-white"></i></div>
+                      </div>
+                      <div><span class="small text-gray-500">December 7, 2019</span>
+                        <p>$290.29 has been deposited into your account!</p>
+                      </div>
+                    </a><a class="dropdown-item d-flex align-items-center" href="#">
+                      <div class="me-3">
+                        <div class="bg-warning icon-circle"><i class="fas fa-exclamation-triangle text-white"></i></div>
+                      </div>
+                      <div><span class="small text-gray-500">December 2, 2019</span>
+                        <p>Spending Alert: We've noticed unusually high spending for your account.</p>
+                      </div>
+                    </a><a class="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
+                  </div>
+                </div>
+              </li>
+              <li class="nav-item dropdown no-arrow mx-1">
+                <div class="nav-item dropdown no-arrow"><a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#"><span class="badge bg-danger badge-counter">7</span><i class="fas fa-envelope fa-fw"></i></a>
+                  <div class="dropdown-menu dropdown-menu-end dropdown-list animated--grow-in">
+                    <h6 class="dropdown-header">alerts center</h6><a class="dropdown-item d-flex align-items-center" href="#">
+                      <div class="dropdown-list-image me-3"><img class="rounded-circle" src="assets/img/avatars/avatar4.jpeg">
+                        <div class="bg-success status-indicator"></div>
+                      </div>
+                      <div class="fw-bold">
+                        <div class="text-truncate"><span>Hi there! I am wondering if you can help me with a problem I've been having.</span></div>
+                        <p class="small text-gray-500 mb-0">Emily Fowler - 58m</p>
+                      </div>
+                    </a><a class="dropdown-item d-flex align-items-center" href="#">
+                      <div class="dropdown-list-image me-3"><img class="rounded-circle" src="assets/img/avatars/avatar2.jpeg">
+                        <div class="status-indicator"></div>
+                      </div>
+                      <div class="fw-bold">
+                        <div class="text-truncate"><span>I have the photos that you ordered last month!</span></div>
+                        <p class="small text-gray-500 mb-0">Jae Chun - 1d</p>
+                      </div>
+                    </a><a class="dropdown-item d-flex align-items-center" href="#">
+                      <div class="dropdown-list-image me-3"><img class="rounded-circle" src="assets/img/avatars/avatar3.jpeg">
+                        <div class="bg-warning status-indicator"></div>
+                      </div>
+                      <div class="fw-bold">
+                        <div class="text-truncate"><span>Last month's report looks great, I am very happy with the progress so far, keep up the good work!</span></div>
+                        <p class="small text-gray-500 mb-0">Morgan Alvarez - 2d</p>
+                      </div>
+                    </a><a class="dropdown-item d-flex align-items-center" href="#">
+                      <div class="dropdown-list-image me-3"><img class="rounded-circle" src="assets/img/avatars/avatar5.jpeg">
+                        <div class="bg-success status-indicator"></div>
+                      </div>
+                      <div class="fw-bold">
+                        <div class="text-truncate"><span>Am I a good boy? The reason I ask is because someone told me that people say this to all dogs, even if they aren't good...</span></div>
+                        <p class="small text-gray-500 mb-0">Chicken the Dog · 2w</p>
+                      </div>
+                    </a><a class="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
+                  </div>
+                </div>
+                <div class="shadow dropdown-list dropdown-menu dropdown-menu-end" aria-labelledby="alertsDropdown"></div>
+              </li>
+              <div class="d-none d-sm-block topbar-divider"></div>
+              <li class="nav-item dropdown no-arrow">
+                <div class="nav-item dropdown no-arrow"><a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#"><span class="d-none d-lg-inline me-2 text-gray-600 small">Valerie Luna</span><img class="border rounded-circle img-profile" src="assets/img/avatars/avatar1.jpeg"></a>
+                  <div class="dropdown-menu shadow dropdown-menu-end animated--grow-in"><a class="dropdown-item" href="#"><i class="fas fa-user fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Profile</a><a class="dropdown-item" href="#"><i class="fas fa-cogs fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Settings</a><a class="dropdown-item" href="#"><i class="fas fa-list fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Activity log</a>
+                    <div class="dropdown-divider"></div><a class="dropdown-item" href="#"><i class="fas fa-sign-out-alt fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Logout</a>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <div class="container">
+          <div class="row">
+            <div class="col-md-8"><img class="img-fluid" src="http://placehold.it/750x500" alt="Alt Text"></div>
+            <div class="col-md-4">
+              <h3 class="my-3">$EMRI</h3>
+              <p>$PLOTI<br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
+              <h3 class="my-3" style="font-size: 18.9px;">Detaje tjera</h3>
+              <ul class="list-unstyled">
+                <li class="text-break">Studio:asasdkasjdkajskdjaksdjaksjdkajsdkjasdj</li>
+                <li>Aktoret: askdjaskdjkasjdk asdasdasdaksdjas</li>
+                <li>Producenti: asdkjaskdjaksdjkasdasldkasldkalskd</li>
+                <li>Box Office: asdkjaskdjkasjdkasjdkasjdkj</li>
+              </ul>
+            </div>
+          </div>
+          <h3 class="my-4">Filmat tjere<br></h3>
+          <div class="row">
+            <div class="col-sm-6 col-md-3 mb-4"><a href="#"><img class="img-fluid" src="http://placehold.it/500x300"></a></div>
+            <div class="col-sm-6 col-md-3 mb-4"><a href="#"><img class="img-fluid" src="http://placehold.it/500x300"></a></div>
+            <div class="col-sm-6 col-md-3 mb-4"><a href="#"><img class="img-fluid" src="http://placehold.it/500x300"></a></div>
+            <div class="col-sm-6 col-md-3 mb-4"><a href="#"><img class="img-fluid" src="http://placehold.it/500x300"></a></div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <div class="card">
+                <div class="card-header">
+                  <h3 style="font-size: 22.4px;margin-bottom: 0px;height: 25px;">Komente</h3>
+                </div>
+                <div class="card-body" style="height: auto;">
+                  <ul class="list-group">
+                    <li class="list-group-item" style="margin-bottom:6px;">
+                      <div class="d-flex media">
+                        <div></div>
+                        <div class="media-body">
+                          <div class="d-flex media" style="overflow:visible;">
+                            <div><img class="mr-3" style="width: 25px; height:25px;" src="assets/img/user-photo4.jpg"></div>
+                            <div style="overflow:visible;" class="media-body">
+                              <div class="row">
+                                <div class="col-md-12">
+                                  <p class="text-break"><a href="#">$komentues1:</a>&nbsp;$Komenti i komentuesit 1. <br>
+                                    <small class="text-muted">August 6, 2016 @ 10:35am </small>
+                                  </p>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                    <li class="list-group-item" style="margin-bottom:6px;">
+                      <div class="d-flex media">
+                        <div></div>
+                        <div class="media-body">
+                          <div class="d-flex media" style="overflow:visible;">
+                            <div><img class="mr-3" style="width: 25px; height:25px;" src="assets/img/user-photo4.jpg"></div>
+                            <div style="overflow:visible;" class="media-body">
+                              <div class="row">
+                                <div class="col-md-12">
+                                  <p><a href="#">Brennan Prill:</a> This guy has been going 100+ MPH on side streets. <br>
+                                    <small class="text-muted">August 6, 2016 @ 10:35am </small>
+                                  </p>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                    <li class="list-group-item" style="margin-bottom:6px;">
+                      <div class="d-flex media">
+                        <div></div>
+                        <div class="media-body">
+                          <div class="d-flex media" style="overflow:visible;">
+                            <div><img class="mr-3" style="width: 25px; height:25px;" src="assets/img/user-photo4.jpg"></div>
+                            <div style="overflow:visible;" class="media-body">
+                              <div class="row">
+                                <div class="col-md-12">
+                                  <p><a href="#">Brennan Prill:</a> This guy has been going 100+ MPH on side streets. <br>
+                                    <small class="text-muted">August 6, 2016 @ 10:35am </small>
+                                  </p>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                  </ul><button class="btn btn-light" style="margin-left: 629px;margin-top: 0px;" type="button">Add Comment</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <footer class="bg-white sticky-footer">
+        <div class="container my-auto">
+          <div class="text-center my-auto copyright"><span>Copyright © Brand 2021</span></div>
+        </div>
+      </footer>
+    </div><a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/js/script.min.js"></script>
+</body>
+
+</html>
--- php/contact.php
+++ php/contact.php
@@ -25,27 +25,17 @@ try{
     die('General Error: '.$e->getMessage());
     }
     try {
-        $subject = 'No Reply-KinoFiek';
-        $message =
-        '<html><body>
-        <h3>Pershendetje ' . $firstname . ",</h3>". '<p>Ne kemi pranuar mesazhin tuaj dhe me ane te kesaj emaile ju informojme qe do te pergjigjjemi ne kerkesen tuaj ne kohen me te shpejte te mundur.</p>
-        <p>Faleminderit qe gjetet kohen per te na kontaktuar.</p>
-        <h3>Kjo eshte nje email e automatizuar, ju lutemi mos beni reply.</h3></body></html>';
-        ;
-        $headers = 'From:kinofiek@gmail.com' . "\r\n";
-        $headers .= "MIME-Version: 1.0\r\n";
-        $headers .= "Content-Type: text/html; charset=utf-8\r\n";
-        if (mail($email, $subject, $message, $headers)) {
-            echo 'U dergua emaili';
-            header("location: ../kontakto.php");
-        }
-        else{
-            echo 'Dergimi i emailes deshtoi';
-        }
-    }
+    require "sendmail.php";
+    $newuser = new SendEmail($email);
+    if($newuser->sendContact($firstname)){
+        unset($newuser);
+        echo 'U dergua emaili';
+        header("location: ../kontakto.php");
+    }else{
+        echo 'Dergimi i emailes deshtoi';
+    }}
     catch (Exception $e) {
         echo $e->getMessage();
     }
-
 }
 ?>
